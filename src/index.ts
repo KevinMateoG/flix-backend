be1daca4dc4b92d6ca915e1b426a04fbad43478a
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2026-09-22 21:51:03 -0500

    feat: add @mux/mux-node dependency and update server setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import express, { type Express, type Request, type Response } from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth.ts';
+import cors from 'cors';
+import moviesRouter from './routes/movies.route.ts';
 
 const app: Express = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Flix Backend OK');
 });
 
-app.listen(3000)
+app.use('/api/auth', toNodeHandler(auth));
 
+app.use('/api/movies', moviesRouter);
 
-console.log(`conection http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
